98f0154ec4946655739f5e6a6baa91a723aa357a
Tareq Imbasher <tareq@meccasoft.com>
2021-04-02 02:32:15 +0300

    some styling love for dark theme and general styling organization

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,9 +1,9 @@
 import { Aurelia, ColorOptions, ConsoleSink, LoggerConfiguration, LogLevel } from 'aurelia';
-import { Window } from './components/window';
 import { converters } from './global-resources';
 import "./jquery.load";
 import "./style/semantic-ui/dist/semantic.css";
 import "./style/semantic-ui/dist/semantic.js";
+import { Window } from './components/window';
 
 Aurelia
     .register(
