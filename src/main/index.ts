5aabc4c4f7b2a50e9902435f26ec61b3eb66251b
Tareq Imbasher <tareq@meccasoft.com>
2021-10-18 03:23:34 +0300

    Enable window transparency

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,15 +9,13 @@ import { WindowManager } from "./window-manager";
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
-// Only run single instance app mode in production builds
-const singleInstanceApp = app.commandLine.hasSwitch("dev") !== true;
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
-  // eslint-disable-line global-require
   app.quit();
 }
 
+// Only run single instance app mode in production builds
+const singleInstanceApp = !app.commandLine.hasSwitch("dev");
 if (singleInstanceApp && !app.requestSingleInstanceLock()) {
   app.quit();
 }
@@ -27,6 +25,12 @@ const trayAndDockManager = new TrayAndDockManager(app, windowManager);
 const environmentManager = new EnvironmentManager(windowManager);
 const eventBus = new IpcEventBus(windowManager);
 const driveService = new DriveService(eventBus);
+
+// For transparency to function correctly on linux
+if (process.platform === "linux") {
+  app.commandLine.appendSwitch("enable-transparent-visuals");
+  app.disableHardwareAcceleration();
+}
 
 app.on("ready", (event, launchInfo) => {
   // Without a timeout, transparency does not seem to work
@@ -40,8 +44,8 @@ app.on("ready", (event, launchInfo) => {
   driveService.start();
 });
 
-// For OSX
 app.on("activate", () => {
+  // For OSX
   if (BrowserWindow.getAllWindows().length === 0) {
     windowManager.createWindow();
   }
